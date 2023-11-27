ccb185447897cd957da0f14e023c53797c9c020a
chemifrandaz <151792763+chemifrandaz@users.noreply.github.com>
2023-11-27 18:56:39 -0300

    Add files via upload

---
+++ prueba2.js
@@ -0,0 +1,37 @@
+/*
+function mensaje(){
+    console.log(hola);
+}
+mensaje();
+mensaje();
+*/
+
+function mayor(n1,n2,n3){
+    if (n1 > n2 && n1 > n3) {
+        console.log(n1);
+    }
+    
+    else if(n2 > n1 && n2 > n3) {
+        console.log (n2);
+    }
+    if (n3 >= n1 && n3 >= n2){
+        console.log(n3)
+    }
+    else{
+        console.log (n3);
+    }
+
+}
+mayor(10,0,100);
+console.log(n3);
+
+function suma(n1, n2){
+ let resultado = (n1 + n2);   
+ return resultado
+
+}
+console.log(10*5);
+
+let variable
+variable = suma(15,10);
+console.log(variable);
